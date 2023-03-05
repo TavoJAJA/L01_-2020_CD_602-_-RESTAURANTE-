1af2b3e8225fe96d4dad34858e60b0841d3d7c8a
Gustavo Cuéllar <92953791+TavoJAJA@users.noreply.github.com>
2023-03-04 22:36:41 -0600

    Add files via upload

---
+++ L01_-2020_CD_602-_-RESTAURANTE--master/L01_2020_CD_602/Controllers/Restaurante.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using L01_2020_CD_602;
+using Microsoft.EntityFrameworkCore;
+
+namespace L01_2020_CD_602.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Restaurante : ControllerBase
+    {
+        private readonly Restaurante _RestauranteContexto; 
+
+        public Restaurante(Restaurante restaurante_Contexto) 
+        {
+            _RestauranteContexto= restaurante_Contexto;
+        }
+    }
+}
---
+++ L01_-2020_CD_602-_-RESTAURANTE--master/L01_2020_CD_602/Modelos/restaurantejeje.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L01_2020_CD_602.Modelos
+{
+    public class restaurantejeje
+    {
+        [Key]
+        public int Clienteid { get; set; }
+        public int nombreCliente { get; set; }
+        public int direccion { get; set; }
+
+    }
+}
